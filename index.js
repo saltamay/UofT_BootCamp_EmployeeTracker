5c7c7fb546851260018d0101fea78dcdae84b50d
Sal Tamay <sal.tamay@outlook.com>
2019-12-01 20:27:26 -0500

    Obtain employee first and last name when prompted

--- index.js
+++ index.js
@@ -16,63 +16,97 @@ db.connect(function (err) {
   console.log('Connected to the database');
 });
 
-db.query(sqlQuery.dropEmployeeTable(), err => {
-  if (err) throw err
-});
-
-db.query(sqlQuery.dropRoleTable(), err => {
-  if (err) throw err
-});
-
-db.query(sqlQuery.dropDepartmentTable(), err => {
-  if (err) throw err
-});
-
-db.query(sqlQuery.createDepartmentTable(), err => {
-  if (err) throw err
-});
-
-db.query(sqlQuery.createRoleTable(), err => {
-  if (err) throw err
-});
-
-db.query(sqlQuery.createEmployeeTable(), err => {
-  if (err) throw err
-});
-
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'function',
-      message: 'What do you want to do?',
-      choices: [
-        'View All Employees',
-        'View All Employees by Department',
-        'View All Employees by Manager'
-      ]
-    },
-    // {
-    //   type: 'list',
-    //   name: 'size',
-    //   message: 'What size do you need?',
-    //   choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
-    //   filter: function (val) {
-    //     return val.toLowerCase();
-    //   }
-    // }
-  ])
-  .then(answers => {
-    console.log(JSON.stringify(answers, null, '  '));
+function resetDB() {
+  db.query(sqlQuery.dropEmployeeTable(), err => {
+    if (err) throw err
   });
 
-// console.table([
-//   {
-//     name: 'View All Employees'
-//   }, {
-//     name: 'View All Employees by Department'
-//   }
-// ]);
+  db.query(sqlQuery.dropRoleTable(), err => {
+    if (err) throw err
+  });
+
+  db.query(sqlQuery.dropDepartmentTable(), err => {
+    if (err) throw err
+  });
+
+  db.query(sqlQuery.createDepartmentTable(), err => {
+    if (err) throw err
+  });
+
+  db.query(sqlQuery.createRoleTable(), err => {
+    if (err) throw err
+  });
+
+  db.query(sqlQuery.createEmployeeTable(), err => {
+    if (err) throw err
+  });
+}
+
+async function getEmployeeInfo() {
+  const answer = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is the employee\'s first name? '
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is the employee\'s last name? '
+      },
+      // {
+      //   type: 'list',
+      //   name: 'department',
+      //   message: 'What do you want to do?',
+      //   choices: [
+      //     'View All Employees',
+      //     'View All Employees by Department',
+      //     'View All Employees by Manager',
+      //     'Add Employee',
+      //     'Remove Employee Role',
+      //     'Update Employee Manager'
+      //   ]
+      // }
+    ]);
+
+  console.log(answer);
+
+  init();
+}
+async function init() {
+  // Reset db
+  resetDB();
+
+  const answer = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What do you want to do?',
+        choices: [
+          'View All Employees',
+          'View All Employees by Department',
+          'View All Employees by Manager',
+          'Add Employee',
+          'Remove Employee Role',
+          'Update Employee Manager'
+        ]
+      }
+    ]);
+
+  switch (answer.action.toLowerCase()) {
+    case 'add employee':
+      getEmployeeInfo();
+      break;
+
+    default:
+      break;
+  }
+}
+
+init();
+
 
 
 
